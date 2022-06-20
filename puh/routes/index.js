1ca5ddca9dfa6346a2c5d5d5df43f2537812e92a
nazartsap <tsap.n.i20@gmail.com>
2022-06-20 22:25:21 +0300

    4.3

--- puh/routes/index.js
+++ puh/routes/index.js
@@ -6,27 +6,48 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 /* Страница Винни Пуха */ 
-router.get('/1', function(req, res, next) { 
-  res.send("<h1>Страница Винни Пуха</h1>") 
+router.get('/vinni', function(req, res, next) {
+  res.render('hero', {
+      title: "Винни Пух",
+      picture: "images/vinni.jpg",
+      desc: "Плюшевый медвежонок, игрушка"
+  });
 });
+
 /* Страница Пятачка */
 router.get('/pig', function(req, res, next) {
-  res.send("<h1>Страница Пятачка</h1>")
+  res.render('hero', {
+      title: "Пятачок",
+      picture: "images/pig.jpg",
+      desc: "Крошечный поросенок, которого зовут Пятачок, и который живет в большом-пребольшом доме"
+  });
 });
 
 /* Страница Кролика */
 router.get('/rabbit', function(req, res, next) {
-  res.send("<h1>Страница Кролика</h1>")
+  res.render('hero', {
+      title: "Кролик",
+      picture: "images/rabbit.jpg",
+      desc: "Кролик, всегда безумно рад видеть Винни-Пуха"
+  });
 });
 
 /* Страница Иа */
 router.get('/iaia', function(req, res, next) {
-  res.send("<h1>Страница Иа</h1>")
+  res.render('hero', {
+      title: "Иа Иа",
+      picture: "images/iaia.jpg",
+      desc: "Грустный ослик, который однажды потерял хвост"
+  });
 });
 
 /* Страница Совы */
 router.get('/sova', function(req, res, next) {
-  res.send("<h1>Страница Совы</h1>")
+  res.render('hero', {
+      title: "Сова",
+      picture: "images/sova.jpg",
+      desc: "Если кто-нибудь что-нибудь о чем-нибудь знает, то это, конечно, Сова"
+  });
 });
 
 module.exports = router;
