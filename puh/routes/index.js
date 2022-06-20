98f541e84d1b58dc54cbe3dd360aeb8884b44019
nazartsap <tsap.n.i20@gmail.com>
2022-06-20 22:09:47 +0300

    4.2

--- puh/routes/index.js
+++ puh/routes/index.js
@@ -9,6 +9,26 @@ router.get('/', function(req, res, next) {
 router.get('/1', function(req, res, next) { 
   res.send("<h1>Страница Винни Пуха</h1>") 
 });
+/* Страница Пятачка */
+router.get('/pig', function(req, res, next) {
+  res.send("<h1>Страница Пятачка</h1>")
+});
+
+/* Страница Кролика */
+router.get('/rabbit', function(req, res, next) {
+  res.send("<h1>Страница Кролика</h1>")
+});
+
+/* Страница Иа */
+router.get('/iaia', function(req, res, next) {
+  res.send("<h1>Страница Иа</h1>")
+});
+
+/* Страница Совы */
+router.get('/sova', function(req, res, next) {
+  res.send("<h1>Страница Совы</h1>")
+});
+
 module.exports = router;
 
 
