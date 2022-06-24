38d03d65c3d9144d3a95d77b874ef4906ba7eaad
nazartsap <tsap.n.i20@gmail.com>
2022-06-24 22:05:35 +0300

    8.1

--- createDB.js
+++ createDB.js
@@ -1,19 +1,13 @@
-var MongoClient = require('mongodb').MongoClient
-var data = require("./data.js").data 
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
 
-const uri = "mongodb://localhost:27017/"
-const client = new MongoClient(uri)
-async function run() {
-    try  {
-        await client.connect();
-        var database = client.db("RTX");
-        database.dropDatabase()
-        database = client.db("RTX");
-        const heroes = database.collection("heroes");
-        const result = await heroes.insertMany(data);
-        console.log(`${result.insertedCount} documents were inserted`);
-    } finally {
-        await client.close();
+var Cat = mongoose.model('Cat', { name: String })
+
+var kitty = new Cat({ name: 'Пушок' })
+kitty.save(function (err) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Мяу')
     }
-}
-run()
+})
