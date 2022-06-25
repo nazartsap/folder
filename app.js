e1a5a735964bc97dfc4e80f7e65a0ed302e7cfb5
nazartsap <tsap.n.i20@gmail.com>
2022-06-25 16:33:52 +0300

    10.2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb://localhost/folder')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var heroes = require('./routes/heroes');
 var app = express();
 
 // view engine setup
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/heroes', heroes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
