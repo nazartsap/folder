bf19fc6d5b4de00fa6d302b0d837e95fc0ed0f48
nazartsap <tsap.n.i20@gmail.com>
2022-06-25 23:05:51 +0300

    12.3

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,47 @@
 var express = require('express')
 var router = express.Router()
 var Hero = require("../models/hero").Hero
-
+var User = require("./../models/User").User
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-		Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
-			req.session.greeting = "Hi!!!"
-			res.render('index', {
-																title: 'Мир новых технологий',
-																menu: menu,
-																counter:req.session.counter 
-														});
-		})
+    Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
+      req.session.greeting = "Hi!!!!"
+      res.render('index', {
+                              title: 'Мир новых технологий',
+                              menu: menu,
+                              counter: req.session.counter
+                          });
+      })
+  });
 
-});
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход'});
   });
-module.exports = router;
+  
+/* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  User.findOne({username:username},function(err,user){
+    if(err) return next(err)
+    if(user){
+      if(user.checkPassword(password)){
+        req.session.user = user
+        res.redirect('/')
+      } else {
+        res.render('logreg', {title: 'Вход', error: 'Пароль не верный'})
+      }
+    } else {
+      var user = new User({username:username,password:password})
+            user.save(function(err,user){
+                if(err) return next(err)
+                req.session.user = user
+                res.redirect('/')
+            })     
+    }
+})
+});
+
+module.exports = router
