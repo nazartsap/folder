c5093c1a7fccfd4abdd08f8c6b2471b46ad611e9
nazartsap <tsap.n.i20@gmail.com>
2022-06-25 16:25:04 +0300

    10.1

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+ 
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/folder')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
