26885eaf84bd3aab1a44dfa9d157cba9cc28e74f
nazartsap <tsap.n.i20@gmail.com>
2022-06-25 16:36:09 +0300

    10.3

--- routes/heroes.js
+++ routes/heroes.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+var Hero = require("../models/hero");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маошрутов, начинающихся с heroes');
 });
 /* Страница героев */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-});
-
+router.get('/:nick', function(req, res, next) {
+    Hero.findOne({nick:req.params.nick}, function(err,hero){
+        if(err) return next(err)
+        if(!hero) return next(new Error("Нет такого героя в этой книжке"))
+        res.render('hero', {
+            title: hero.title,
+            picture: hero.avatar,
+            desc: hero.desc
+        })
+    })
+})
 module.exports = router;
