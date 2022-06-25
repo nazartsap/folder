da0fdfb05b05e38dccb6add59be3f99c0ea23c9c
nazartsap <tsap.n.i20@gmail.com>
2022-06-25 19:17:42 +0300

    11.1

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
- 
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/folder')
-
+var session = require("express-session")
+var heroesRouter = require('./routes/heroes');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var heroes = require('./routes/heroes');
+
 var app = express();
 
 // view engine setup
-app.engine('ejs', require('ejs-locals'));
+app.engine('ejs',require('ejs-locals'));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -23,10 +24,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(session({
+  secret: "VinniIsHero",
+  cookie:{maxAge:60*1000}
+}))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/heroes', heroes);
+app.use('/heroes', heroesRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Hero = require("../models/hero").Hero
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+		Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
+				res.render('index', {
+																title: 'Мир новых технологий',
+																menu: menu
+														});
+		})
 
-/* Страница 3090 */ 
-router.get('/3090', function(req, res, next){
-  res.render('hero', {
-    title: "RTX3090",
-    picture: "images/3090.png",
-    desc: 'Оспечивают оптимальную производительность в топовых играх благодаря возможностям Ampere — архитектуры NVIDIA RTX второго поколения.'
-  })
-});
-
-/* Страница 3070ti */ 
-router.get('/3070ti', function(req, res, next){
-  res.render('hero', {
-    title: "RTX3070ti",
-    picture: "images/3070ti.png",
-    desc: 'Оспечивают оптимальную производительность в топовых играх благодаря возможностям Ampere — архитектуры NVIDIA RTX второго поколения.'
-  })
-});
-
-/* Страница 3070 */ 
-router.get('/3070', function(req, res, next){
-  res.render('hero', {
-    title: "RTX3070",
-    picture: "images/3070.png",
-    desc: 'Оспечивают оптимальную производительность в топовых играх благодаря возможностям Ampere — архитектуры NVIDIA RTX второго поколения.'
-  })
 });
 
 module.exports = router;
