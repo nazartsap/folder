4806a3d4a9150449674fb14f1b74de2b283e8014
nazartsap <tsap.n.i20@gmail.com>
2022-06-25 22:12:40 +0300

    11.5

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/folder')
 var session = require("express-session")
+var Hero = require("./models/hero").Hero
 
 var heroesRouter = require('./routes/heroes');
 var indexRouter = require('./routes/index');
@@ -36,8 +37,20 @@ store: MongoStore.create({mongoUrl: 'mongodb://localhost/folder'})
 app.use(function(req,res,next){
   req.session.counter = req.session.counter + 1 || 1
   next()
+})   
+
+app.use(function(req,res,next){
+  res.locals.nav = []
+
+  Hero.find(null,{_id:0,title:1,nick:1},function(err,result){
+      if(err) throw err
+      res.locals.nav = result
+      next()
   })
-  
+})
+
+app.use(require("./middleware/createMenu.js"))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/heroes', heroesRouter);
